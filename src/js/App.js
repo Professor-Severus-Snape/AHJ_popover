62e229020c9ac819325b215e9cc51a375baaf0ac
Julia Fokanova <oncologmma@mail.ru>
2024-07-22 02:12:13 +0300

    add logic

--- src/js/App.js
+++ src/js/App.js
@@ -1,9 +1,23 @@
+import Button from '../components/button/button';
+import Popover from '../components/popover/popover';
+
 export default class App {
   constructor() {
+    this.container = document.querySelector('.container');
 
+    this.button = new Button();
+    this.popover = new Popover();
   }
 
   init() {
+    this.container.append(this.button.element);
+    this.container.append(this.popover.element);
 
+    this.button.element.addEventListener('click', this.onBtnClick.bind(this));
+  }
+
+  onBtnClick(event) {
+    this.popover.changeVisibility();
+    this.popover.setPosition(event.target);
   }
 }
